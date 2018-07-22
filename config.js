1b309ed3adf1ac3c4f4f9153cc6b69071aa434aa
Nye Warburton <nyewarburton@gmail.com>
2018-07-22 14:04:01 -0700

    first push of website template to live cms

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: 'NyeToon', // Required
+  author: 'NyeToon', // Required
+  tagline: 'Sometimes Cartoonist',
+  primaryColor: '#27479f', // Required
+  showHeaderImage: false,
+  showShareButtons: true,
+  postsPerPage: 5, // Required
+  social: {
+    website: 'https://nyetoon.net',
+    github: 'https://github.com/NyeGuy/NyeToon',
+    twitter: 'https://twitter.com/nyetoon',
+    facebook: 'https://facebook.com/nyetoon'
+  }
+};
