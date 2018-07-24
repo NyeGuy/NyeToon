6607477f31711e5274eb0ef6bf42c78f629fc2fd
Nye Warburton <nyewarburton@gmail.com>
2018-07-24 14:16:46 -0700

    adding feature header image.

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ module.exports = {
   postsPerPage: 5, // Required
   social: {
     website: 'https://rarebits.io',
+    website: 'https://rarebits.io',
     github: 'https://github.com/NyeGuy/NyeToon',
     twitter: 'https://twitter.com/nyetoon/',
     instagram: 'https://www.instagram.com/nyetoon/'
