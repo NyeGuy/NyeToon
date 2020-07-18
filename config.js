cfbb37a67d613a749ae691eeeb8d3ea9d64a8974
Nye <nyewarburton@gmail.com>
2020-07-17 21:39:49 -0700

    updating info

--- config.js
+++ config.js
@@ -1,15 +1,14 @@
 module.exports = {
   title: 'NyeToon', // Required
-  author: 'Nyetoon', // Required
+  author: 'Nye Warburton', // Required
   tagline: 'Comics by Nye',
   primaryColor: '#27479f', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 10, // Required
   social: {
-    website: 'https://nyetoon.net/tableofcontents',
-    website: 'https://nyetoon.net/tableofcontents',
+    website: 'https://nyewarburton.com',
     //github: 'https://github.com/NyeGuy/NyeToon',
-   // twitter: 'https://twitter.com/nyetoon/',
+   // twitter: 'https://twitter.com/nyewarburton/',
   }
 };
